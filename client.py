3056f771733bd60a79b3fd95627974433aa2cad4
mnsnow <lixx2813@umn.edu>
2017-12-31 20:44:56 -0600

    local network established

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket
 
-host = ''
+host = '192.168.1.34'
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,7 +13,7 @@ while message != 'q':
     s.send(message.encode())
     data1 = s.recv(2048)
     data = data1.decode()
-    print('Received from server' + str(data))
+    print('Received from server: ' + str(data))
     message = input(' --> ')
 
 s.close()
