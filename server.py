caef8fa031de8d5225ccd13a2302ce2dd19e36d3
mnsnow <lixx2813@umn.edu>
2018-01-11 14:55:46 -0600

    client/server can transfer infomation and correctly store remote information into local txt file

--- server.py
+++ server.py
@@ -1,43 +1,7 @@
-# import socket
-# import select
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#
-# host = ''
-# port = 5555
-#
-# s.bind((host, port))
-#
-# s.listen(5)
-#
-# c, addr = s.accept()
-#
-# print('Connection from: '+ str(addr))
-#
-# while 1:
-#     data1 = c.recv(1024)
-#     data = data1.decode()
-#     if not data:
-#         print('break while')
-#         break
-#     print('from connected user:' + str(data))
-#     data = str(data).upper()
-#     print('sending:' + str(data))
-#     c.send(data.encode())
-#
-# c.close()
-# print('55')
-
-# # Use this to find local ip address
-# import socket
-# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# s.connect(("8.8.8.8", 80))
-# print(s.getsockname()[0])
-# s.close()
-
-
-
 import socket
 import select
+import time
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = ''
@@ -48,22 +12,127 @@ s.listen(5)
 
 c, addr = s.accept()
 
-print('Connection from: '+ str(addr))
-
 while 1:
+
+    # Sending data
     with open('connection.txt','r') as f:
         f.seek(0)
         for line in f:
-            c.send(str(line).encode())
-            print('sending:' + str(line))
+            if 'USER_NAME' in line:
+                user_name_01 = str(line)
+            if 'PLAYER_NAME' in line:
+                player_name_01 = str(line)
+            if 'USER_CHARACTER_HP' in line:
+                user_character_hp_01 = str(line)
+
+    time.sleep(0.3)
+
+    with open('connection.txt','r') as f:
+        f.seek(0)
+        for line in f:
+            if 'USER_NAME' in line:
+                user_name_02 = str(line)
+            if 'PLAYER_NAME' in line:
+                player_name_02 = str(line)
+            if 'USER_CHARACTER_HP' in line:
+                user_character_hp_02 = str(line)
+
+    if user_name_01 != user_name_02:
+        c.send(((str(user_name_02)[:-1]+'||')*10).encode())
+    if player_name_01 != player_name_02:
+        c.send(((str(player_name_02)[:-1]+'||')*10).encode())
+    if user_character_hp_01 != user_character_hp_02:
+        c.send(((str(user_character_hp_02)[:-1]+'||')*10).encode())
+
+    c.send(('lalala'*10).encode())
+    # Reciving data
     data1 = c.recv(1024)
     data = data1.decode()
     if not data:
         print('break while')
         break
-    print('from connected user:' + str(data))
+
+    user_name = ''
+    player_name = ''
+    user_character_hp = ''
+    # get variables from data
+    if 'USER_NAME' in data:
+        aaa = data[data.find('USER_NAME'):]
+        bbb = aaa[:aaa.find('||')]
+        user_name = str(bbb.replace('USER_NAME = ', ''))
+        print('USER_NAME---------: ' + user_name + '.')
+
+    if 'PLAYER_NAME' in data:
+        aaa = data[data.find('PLAYER_NAME'):]
+        bbb = aaa[:aaa.find('||')]
+        player_name = str(bbb.replace('PLAYER_NAME = ', ''))
+        print('PLAYER_NAME---------: ' + player_name + '.')
+
+    if 'USER_CHARACTER_HP' in data:
+        aaa = data[data.find('USER_CHARACTER_HP'):]
+        bbb = aaa[:aaa.find('||')]
+        user_character_hp = str(bbb.replace('USER_CHARACTER_HP = ', ''))
+        print('USER_CHARACTER_HP---------: ' + user_character_hp + '.')
+
+
+
+    # Writing into the file
+    with open('connection.txt','a+') as f:
+        f.seek(0)
+        x = f.readlines()
+
+        #write user_name
+        if user_name != '':
+            y = 1
+            f.seek(0)
+            for line in f:
+                if 'USER_NAME' not in line:
+                    y += 1
+                else:
+                    break
+            x[y-1] = 'USER_NAME = ' + user_name + '\n'
+
+        #write player_name
+        if player_name != '':
+            y = 1
+            f.seek(0)
+            for line in f:
+                if 'PLAYER_NAME' not in line:
+                    y += 1
+                else:
+                    break
+            x[y-1] = 'PLAYER_NAME = ' + player_name + '\n'
+
+        #write user.character_card.health
+        if user_character_hp != '':
+            y = 1
+            f.seek(0)
+            for line in f:
+                if 'USER_CHARACTER_HP' not in line:
+                    y += 1
+                else:
+                    break
+            x[y-1] = 'USER_CHARACTER_HP = ' + user_character_hp + '\n'
+
+
+    with open('connection.txt','w') as f:
+        f.writelines(x)
+
 
 
 
 c.close()
-print('55')
+
+
+
+
+
+
+
+
+
+
+
+
+
+# --
